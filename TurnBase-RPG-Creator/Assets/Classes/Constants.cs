ab668293bd3976ed8fb3a1c7349e9394b1b502d6
leonelpaulino <leonel.paulino18@gmail>
2015-10-03 11:24:50 -0300

    Agregando constantes para el mapay Arreglando los espacios entre objetos en el mapa

---
+++ TurnBase-RPG-Creator/Assets/Classes/Constants.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Constantes de TurnBase-RPG-Creator.
+/// </summary>
+public class Constatnt{
+	/// <summary>
+	/// La altura maxima del mapa
+	/// </summary>
+	public const int MAX_MAP_HEIGTH = 10;
+	/// <summary>
+	/// Ancho maximo del mapa
+	/// </summary>
+	public const int MAX_MAP_WIDTH = 25;
+	/// <summary>
+	/// Altura Minima del mapa
+	/// </summary>
+	public const int MIN_MAP_HEIGTH = 5;
+	/// <summary>
+	/// Ancho Minimo del mapa.
+	/// </summary>
+	public const int MIN_MAP_WIDTH = 5;
+}
--- TurnBase-RPG-Creator/Assets/Editor/MapWindow.cs
+++ TurnBase-RPG-Creator/Assets/Editor/MapWindow.cs
@@ -10,15 +10,31 @@ public class MapWindow : EditorWindow {
 		map = new Map ();
 		map.Heigth = 10;
 		map.Width=10;
-		map.Name = "new_mapa1";
+		map.Name = "new_map1";
 	}
 	void OnGUI () {
 		if (map == null)
 			this.Init ();
 		GUILayout.Label ("Ajustes Basicos", EditorStyles.boldLabel);
-		map.Name = EditorGUILayout.TextField ("Nombre Del Mapa",map.Name);
-		map.Width = EditorGUILayout.IntField ("Ancho", map.Width);
-		map.Heigth = EditorGUILayout.IntField ("Alto", map.Heigth);
+		if (map.Name.Length == 0 )
+			map.Name = EditorGUILayout.TextField ("Nombre Del Mapa","new_map1");
+		else 
+			map.Name = EditorGUILayout.TextField ("Nombre Del Mapa",map.Name);
+
+		if (map.Width > Constatnt.MAX_MAP_WIDTH)
+			map.Width = EditorGUILayout.IntField ("Ancho",25);
+		else if (map.Width < Constatnt.MIN_MAP_WIDTH )
+			map.Width = EditorGUILayout.IntField ("Ancho",5);
+		else
+			map.Width = EditorGUILayout.IntField ("Ancho", map.Width);
+
+		if ( map.Heigth > Constatnt.MAX_MAP_HEIGTH) 
+			map.Heigth = EditorGUILayout.IntField ("Alto", 10);
+		else if ( map.Heigth < Constatnt.MIN_MAP_HEIGTH) 
+			map.Heigth = EditorGUILayout.IntField ("Alto", 5);
+		else 
+			map.Heigth = EditorGUILayout.IntField ("Alto", map.Heigth);
+
 		if (GUILayout.Button ("Crear Mapa")) {
 			CreateMap();
 		}
@@ -31,12 +47,18 @@ public class MapWindow : EditorWindow {
 		Camera.main.orthographicSize = 5; // Ajusta el tamaño de la camara ( la cantidad de espacio que va enfocar)
 		Camera.main.transform.localPosition = new Vector3((float)(map.Width-1)/2,(float)(map.Heigth-1)/2,-10); // Posiciona la camara en el centro del mapa
 		Camera.main.backgroundColor = Color.black;
-		Object prefab = AssetDatabase.LoadAssetAtPath(@"Assets/Resources/Floors/Floor1.prefab", typeof(GameObject)); // Carga el prefab que esta por defecto para crear el mapa
+		Object prefab = AssetDatabase.LoadAssetAtPath(@"Assets/Resources/Floors/Dark.prefab", typeof(GameObject)); // Carga el prefab que esta por defecto para crear el mapa
+		Object prefab2 = AssetDatabase.LoadAssetAtPath(@"Assets/Resources/Floors/Light.prefab", typeof(GameObject)); // Carga el prefab que esta por defecto para crear el mapa
 		for (int i =0; i < map.Width;i++){
 			for(int j = 0; j < map.Heigth;j++){
 				Vector2 position = new Vector2(i,j);
-				GameObject clone =  Instantiate(prefab, position, Quaternion.identity) as GameObject; // Agrega un objeto nuevo a la scene.
-				clone.transform.localScale= new Vector2((float)3.049475,(float)3.070369); // Ajusta el scale del objeto.
+				GameObject clone = new GameObject();
+				if ( ( i+j ) % 2 == 0 ) 
+					clone = Instantiate(prefab, position, Quaternion.identity) as GameObject; // Agrega un objeto nuevo a la scene.
+				else 
+					clone = Instantiate(prefab2, new Vector3(i,j,0f), Quaternion.identity) as GameObject;
+				//clone.transform.localScale= new Vector2((float)3.049475,(float)3.070369); // Ajusta el scale del objeto.
+				DestroyImmediate(GameObject.Find("New Game Object"));
 			}
 		}
 		EditorApplication.SaveScene("Assets/Maps/"+map.Name+".unity");// Guarda la scene.
