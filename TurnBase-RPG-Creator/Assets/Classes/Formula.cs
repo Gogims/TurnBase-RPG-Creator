f4ffcc32b0a43b9876168b8dc352aea03d504203
Gogims <gogims@gmail.com>
2015-10-07 17:04:34 -0400

    Mejorando la formula de curva de exp y documentando código

--- TurnBase-RPG-Creator/Assets/Classes/Formula.cs
+++ TurnBase-RPG-Creator/Assets/Classes/Formula.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Formula : MonoBehaviour
 {
     /// <summary>
@@ -15,7 +16,7 @@ public class Formula : MonoBehaviour
     public int ExtraValue = 20;
 
     /// <summary>
-    /// 
+    /// Valor para hacer crecer los últimos niveles
     /// </summary>
     public int Acceleration = 30;
 
@@ -24,27 +25,61 @@ public class Formula : MonoBehaviour
     /// </summary>
     public bool updated;
 
+    /// <summary>
+    /// Valor mínimo de los sliders de la curva de experiencia
+    /// </summary>
     public const int MinValue = 10;
 
+    /// <summary>
+    /// Valor máximo de los sliders de la curva de experiencia
+    /// </summary>
     public const int MaxValue = 50;
 
+    /// <summary>
+    /// Máximo nivel que un personaje puede llegar
+    /// </summary>
+    public const int MaxLevel = 99;
+
+    /// <summary>
+    /// Estructura que contiene el (nivel, cantidad de xp para subir al próximo nivel)
+    /// </summary>
     private Dictionary<int, int> Growth;
 
+    /// <summary>
+    /// Convierte la aceleración en decimal
+    /// </summary>
+    private double acc
+    {
+        get { return (double)Acceleration / 100; }
+    }
+
+    /// <summary>
+    /// Constructor que agrega el nivel máximo de items en el diccionario y le asigna un valor por defecto
+    /// </summary>
     public Formula()
     {
-        Growth = new Dictionary<int, int>(99);
+        Growth = new Dictionary<int, int>(MaxLevel);
 
-        for (int i = 1; i <= 99; i++)
+        for (int i = 1; i <= MaxLevel; i++)
         {
             Growth.Add(i, GetLevelValue(i));
         }
     }
 
+    /// <summary>
+    /// Busca en el diccionario cuanta experiencia se necesita para pasar el próximo nivel
+    /// </summary>
+    /// <param name="Level">Nivel que se desea acceder</param>
+    /// <returns>Cantidad de nivel necesitado para el próximo nivel</returns>
     public int GetValue(int Level)
     {
         return Growth[Level];
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns>Valores necesitados para pasar al próximo nivel</returns>
     public IEnumerable<int> GenerateCurve()
     {
         foreach (var item in Growth)
@@ -53,14 +88,22 @@ public class Formula : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Actualiza el diccionario completo a partir del valor base, valor extra y la acceleración
+    /// </summary>
     public void UpdateValue()
     {
         for (int i = 1; i <= Growth.Count; i++)
         {
             Growth[i] = GetLevelValue(i);
         }
-    }    
+    }
 
+    /// <summary>
+    /// Fórmula para calcular la cantidad de experiencia necesitada para pasar al próximo nivel, dado un nivel
+    /// </summary>
+    /// <param name="Level">Nivel del personaje</param>
+    /// <returns>Cantidad de experiencia necesitada para pasar al próximo nivel</returns>
     private int GetLevelValue(int Level)
     {
         int previouslevel = 0;
@@ -68,6 +111,6 @@ public class Formula : MonoBehaviour
         if (Level > 1)
             previouslevel = Growth[Level - 1];
 
-        return previouslevel + BaseValue + (Level * ExtraValue) ^ (1 + (Acceleration / 100));
+        return previouslevel + BaseValue + (Level * ExtraValue) + (int)(acc * Level * ExtraValue);
     }
 }
--- TurnBase-RPG-Creator/Assets/Editor/ExpCurve.cs
+++ TurnBase-RPG-Creator/Assets/Editor/ExpCurve.cs
@@ -5,29 +5,46 @@ using UnityEditor;
 public class ExpCurve : EditorWindow
 {
     Formula curve;
+    Texture2D tex2d;
 
     public void Init()
     {
         curve = new Formula();
+        tex2d = new Texture2D(0, 0);
+        tex2d.SetPixel(0, 0, Color.yellow);
     }
 
     void OnGUI()
-    {
-        EditorGUILayout.LabelField("Base Value");
-        curve.BaseValue = EditorGUILayout.IntSlider(curve.BaseValue, Formula.MinValue, Formula.MaxValue);
+    {        
         int i = 0;
+        int graphHeight = curve.GetValue(Formula.MaxLevel) / 1000;
 
         foreach (var item in curve.GenerateCurve())
         {
             i++;
-            //EditorGUILayout.LabelField(item.ToString());
 
-            GUI.Box(new Rect(i, 0, 1, item), "test");
-        }        
+            GUI.Box(new Rect(i*10, graphHeight + 20, 10, -item/1000), tex2d);
+        }
+
+        EditorGUI.LabelField(new Rect(0, graphHeight + 60, 100, 20), "Base Value");
+        curve.BaseValue = EditorGUI.IntSlider(new Rect(100, graphHeight + 60, 300, 20), curve.BaseValue, Formula.MinValue, Formula.MaxValue);
+
+        EditorGUI.LabelField(new Rect(0, graphHeight + 80, 100, 20), "Extra Value");
+        curve.ExtraValue = EditorGUI.IntSlider(new Rect(100, graphHeight + 80, 300, 20), curve.ExtraValue, Formula.MinValue, Formula.MaxValue);
+
+        EditorGUI.LabelField(new Rect(0, graphHeight + 100, 100, 20), "Acceleration");
+        curve.Acceleration = EditorGUI.IntSlider(new Rect(100, graphHeight + 100, 300, 20), curve.Acceleration, Formula.MinValue, Formula.MaxValue);
+
+        for (i = 1; i <= Formula.MaxLevel; i = i+3)
+        {
+            EditorGUI.LabelField(new Rect(0, graphHeight + 120 + i*5, 300, 20), "Lv. " + i.ToString() + ": " + curve.GetValue(i) + " xp");
+            EditorGUI.LabelField(new Rect(300, graphHeight + 120 + i*5, 300, 20), "Lv. " + (i+1).ToString() + ": " + curve.GetValue(i+1) + " xp");
+            EditorGUI.LabelField(new Rect(600, graphHeight + 120 + i*5, 300, 20), "Lv. " + (i+2).ToString() + ": " + curve.GetValue(i+2) + " xp");
+        }
     }
 
     void Update()
     {
-
+        curve.UpdateValue();
     }
 }
